7abf7cd4bee5572ae410129329cff37ede817c65
Mark Klass <mark_christian@zoho.com>
2021-08-04 05:54:24 +0800

    add init script for Rocky Linux 8 - Docker, Docker-Compose

---
+++ init_rocky.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf update -y --allowerasing --nobest
+dnf upgrade -y --allowerasing --nobest
+
+dnf install docker-ce curl wget --allowerasing --nobest -y
+curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url | grep docker-compose-Linux-x86_64 | cut -d '"' -f 4 | wget -qi -
+
+mv docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+systemctl enable docker
+systemctl start docker
+
+echo ""
+echo "----------------"
+printf "Almost done! Run the following:\n  usermod -aG docker $USER\nThen relog in to start using Docker and Docker-Compose.\n"
+echo "----------------"
